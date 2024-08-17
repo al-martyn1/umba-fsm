70dc85ff99cee19216ca2ca8e85f655617c239ed
Alexandr Martynov <mag@unicont.com>
2024-08-17 15:32:24 +0300

    Порефакторил, вынес вспомогательные функции

--- src/umba-fsm/test_009.cpp
+++ src/umba-fsm/test_009.cpp
@@ -17,7 +17,8 @@
 #include "umba/iterator.h"
 #include "umba/the.h"
 //
-
+#include "utils.h"
+//
 #include "marty_cpp/src_normalization.h"
 //
 
@@ -45,70 +46,6 @@ using std::cerr;
 
 
 
-template<typename StringType>
-StringType getTokenKindString(umba::tokenizer::payload_type p)
-{
-    switch(p)
-    {
-        case UMBA_TOKENIZER_TOKEN_UNEXPECTED                       : return umba::string_plus::make_string<StringType>("unxp");
-        case UMBA_TOKENIZER_TOKEN_LINEFEED                         : return umba::string_plus::make_string<StringType>("");
-        case UMBA_TOKENIZER_TOKEN_SPACE                            : return umba::string_plus::make_string<StringType>("");
-        case UMBA_TOKENIZER_TOKEN_TAB                              : return umba::string_plus::make_string<StringType>("");
-        case UMBA_TOKENIZER_TOKEN_IDENTIFIER                       : return umba::string_plus::make_string<StringType>("ident");
-        case UMBA_TOKENIZER_TOKEN_SEMIALPHA                        : return umba::string_plus::make_string<StringType>("");
-        // https://en.wikipedia.org/wiki/Bracket
-        case UMBA_TOKENIZER_TOKEN_CURLY_BRACKET_OPEN               : return umba::string_plus::make_string<StringType>("curly");
-        case UMBA_TOKENIZER_TOKEN_CURLY_BRACKET_CLOSE              : return umba::string_plus::make_string<StringType>("curly");
-        case UMBA_TOKENIZER_TOKEN_ROUND_BRACKET_OPEN               : return umba::string_plus::make_string<StringType>("round");
-        case UMBA_TOKENIZER_TOKEN_ROUND_BRACKET_CLOSE              : return umba::string_plus::make_string<StringType>("round");
-        case UMBA_TOKENIZER_TOKEN_ANGLE_BRACKET_OPEN               : return umba::string_plus::make_string<StringType>("angle");
-        case UMBA_TOKENIZER_TOKEN_ANGLE_BRACKET_CLOSE              : return umba::string_plus::make_string<StringType>("angle");
-        case UMBA_TOKENIZER_TOKEN_SQUARE_BRACKET_OPEN              : return umba::string_plus::make_string<StringType>("square");
-        case UMBA_TOKENIZER_TOKEN_SQUARE_BRACKET_CLOSE             : return umba::string_plus::make_string<StringType>("square");
-        case UMBA_TOKENIZER_TOKEN_OPERATOR_MULTI_LINE_COMMENT_START: return umba::string_plus::make_string<StringType>("cmnt");
-        case UMBA_TOKENIZER_TOKEN_OPERATOR_MULTI_LINE_COMMENT_END  : return umba::string_plus::make_string<StringType>("cmnt");
-        case UMBA_TOKENIZER_TOKEN_PP_START                         : return umba::string_plus::make_string<StringType>("pp");
-        case UMBA_TOKENIZER_TOKEN_PP_END                           : return umba::string_plus::make_string<StringType>("pp");
-
-        //case : return umba::string_plus::make_string<StringType>("");
-        default:
-
-            if (p>=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_FIRST && p<=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_LAST)
-            {
-                if (p&UMBA_TOKENIZER_TOKEN_FLOAT_FLAG)
-                    return umba::string_plus::make_string<StringType>("num");
-                else
-                    return umba::string_plus::make_string<StringType>("num");
-            }
-
-            if (p>=UMBA_TOKENIZER_TOKEN_OPERATOR_SINGLE_LINE_COMMENT_FIRST && p<=UMBA_TOKENIZER_TOKEN_OPERATOR_SINGLE_LINE_COMMENT_LAST)
-                return umba::string_plus::make_string<StringType>("cmnt");
-
-            //if (p>=UMBA_TOKENIZER_TOKEN_NUMBER_USER_LITERAL_FIRST && p<=UMBA_TOKENIZER_TOKEN_NUMBER_USER_LITERAL_LAST)
-            if (p>=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_FIRST && p<=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_LAST)
-                return umba::string_plus::make_string<StringType>("num");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_OPERATOR_FIRST && p<=UMBA_TOKENIZER_TOKEN_OPERATOR_LAST)
-                return umba::string_plus::make_string<StringType>("op");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_STRING_LITERAL_FIRST && p<=UMBA_TOKENIZER_TOKEN_STRING_LITERAL_LAST)
-                return umba::string_plus::make_string<StringType>("str");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_KEYWORD_SET1_FIRST && p<=UMBA_TOKENIZER_TOKEN_KEYWORD_SET1_LAST)
-                return umba::string_plus::make_string<StringType>("kwd1");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_KEYWORD_SET2_FIRST && p<=UMBA_TOKENIZER_TOKEN_KEYWORD_SET2_LAST)
-                return umba::string_plus::make_string<StringType>("kwd2");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_KEYWORD_SET3_FIRST && p<=UMBA_TOKENIZER_TOKEN_KEYWORD_SET3_LAST)
-                return umba::string_plus::make_string<StringType>("kwd3");
-
-            if (p>=UMBA_TOKENIZER_TOKEN_KEYWORD_SET4_FIRST && p<=UMBA_TOKENIZER_TOKEN_KEYWORD_SET4_LAST)
-                return umba::string_plus::make_string<StringType>("kwd4");
-
-            return umba::string_plus::make_string<StringType>("");
-    }
-}
 
 /*
   color:blue
@@ -278,33 +215,11 @@ struct TokenInfo
 };
 
 
-inline
-auto makeTokenText(umba::tokenizer::payload_type tokenType, umba::iterator::TextPositionCountingIterator<char> b, umba::iterator::TextPositionCountingIterator<char> e)
-{
-    if (tokenType==UMBA_TOKENIZER_TOKEN_LINEFEED)
-    {
-        return std::string("\n");
-    }
-
-    if (tokenType==UMBA_TOKENIZER_TOKEN_LINE_CONTINUATION)
-    {
-        return std::string("\\\n");
-    }
-
-    if (tokenType==UMBA_TOKENIZER_TOKEN_OPERATOR_MULTI_LINE_COMMENT_START)
-    {
-        return marty_cpp::normalizeCrLfToLf(umba::iterator::makeString(b, e));
-    }
-
-    return umba::iterator::makeString(b, e);
-}
-
-
 std::string inputFilename;
 
 
 template<typename StreamType, typename InputIteratorType>
-StreamType& printToken(StreamType &ss, umba::tokenizer::payload_type tokenType, InputIteratorType b, InputIteratorType e)
+StreamType& printTokenHtml(StreamType &ss, umba::tokenizer::payload_type tokenType, InputIteratorType b, InputIteratorType e)
 {
     auto kindStr   = getTokenKindString<std::string>(tokenType);
     auto tokenText = makeTokenText(tokenType, b, e);
@@ -327,56 +242,11 @@ StreamType& printToken(StreamType &ss, umba::tokenizer::payload_type tokenType, 
 }
 
 template<typename StreamType>
-StreamType& printToken(StreamType &ss, const TokenInfo &ti)
+StreamType& printTokenHtml(StreamType &ss, const TokenInfo &ti)
 {
-    return printToken(ss, ti.tokenType, ti.b, ti.e);
+    return printTokenHtml(ss, ti.tokenType, ti.b, ti.e);
 
 }
-
-// template<typename StreamType>
-// StreamType& printToken(StreamType &ss, const TokenInfo &ti, umba::tokenizer::payload_type tokenType)
-// {
-// }
-
-
-
-
-void printError(umba::tokenizer::payload_type tokenType, umba::iterator::TextPositionCountingIterator<char> it, umba::iterator::TextPositionCountingIterator<char> itEnd)
-{
-     UMBA_USED(tokenType);
-
-     if (it==itEnd)
-     {
-         cout << "Unexpected end of file\n";
-         return;
-     }
-
-     auto errPos = it.getPosition(true); // с поиском конца строки (а вообще не надо пока, но пусть)
-     std::string erroneousLineText = umba::iterator::makeString(it.getLineStartIterator(), it.getLineEndIterator());
-     cout << "Unexpected at " << inputFilename << ":" << errPos.toString<std::string>() << "\n";
-     cout << "Line:" << erroneousLineText << "\n";
-     auto errMarkerStr = std::string(erroneousLineText.size(), ' ');
-     if (errPos.symbolOffset>=errMarkerStr.size())
-         errMarkerStr.append(1,'^');
-     else
-         errMarkerStr[errPos.symbolOffset] = '^';
-     cout << "    |" << errMarkerStr << "|\n";
-
-     if (it!=itEnd)
-     {
-         char ch = *it;
-         cout << "ch: " << umba::escapeStringC(std::string(1,ch)) << "\n";
-     }
-}
-
-void printError(umba::tokenizer::payload_type tokenType, umba::iterator::TextPositionCountingIterator<char> it, umba::iterator::TextPositionCountingIterator<char> itEnd, const char* srcFile, int srcLine)
-{
-    printError(tokenType, it, itEnd);
-     cout << "At " << srcFile << ":" << srcLine << "\n";
-}
-
-
-
 
 
 
@@ -583,7 +453,7 @@ int main(int argc, char* argv[])
     {
         for(const auto &ti : bufferedTokens)
         {
-            printToken(oss, ti);
+            printTokenHtml(oss, ti);
         }
 
         bufferedTokens.clear();
@@ -678,7 +548,7 @@ int main(int argc, char* argv[])
                                      }
                                  }
 
-                                 printToken(oss, tokenType, b, e);
+                                 printTokenHtml(oss, tokenType, b, e);
 
                                  if (tokenType>=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_FIRST && tokenType<=UMBA_TOKENIZER_TOKEN_NUMBER_LITERAL_LAST)
                                  {
@@ -713,7 +583,7 @@ int main(int argc, char* argv[])
 
     tokenizer.unexpectedHandler = [&](tokenizer_type &tokenizer, InputIteratorType it, InputIteratorType itEnd, const char* srcFile, int srcLine) -> bool
                              {
-                                 printError(UMBA_TOKENIZER_TOKEN_UNEXPECTED, it, itEnd, srcFile, srcLine);
+                                 printError(std::cout, inputFilename, UMBA_TOKENIZER_TOKEN_UNEXPECTED, it, itEnd, srcFile, srcLine);
                                  return false;
                              };
 
