a8633365785365edd6e4953d8e82ab03924716e4
al-martyn1 <amart@mail.ru>
2024-10-05 15:00:04 +0300

    Обновил precommit

--- _precommit.bat
+++ _precommit.bat
@@ -1,2 +1,5 @@
+@set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
 @call "%~dp0\.bat\fix_tabs.bat"
+@set CLOCMD=md_
+@call "%~dp0\.bat\run_clock.bat"
 @call "%~dp0\.bat\gen-doc.bat"
